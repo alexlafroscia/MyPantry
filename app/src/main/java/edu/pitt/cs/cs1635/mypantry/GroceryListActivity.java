9c983cf28031525c0db2253df0d69abb07ed1312
Alex LaFroscia <alex@lafroscia.com>
2016-04-12 02:31:50 -0400

    Pull grocery list from greenDao

--- app/src/main/java/edu/pitt/cs/cs1635/mypantry/GroceryListActivity.java
+++ app/src/main/java/edu/pitt/cs/cs1635/mypantry/GroceryListActivity.java
@@ -2,31 +2,28 @@ package edu.pitt.cs.cs1635.mypantry;
 
 import android.app.Dialog;
 import android.app.DialogFragment;
-import android.content.Context;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.Snackbar;
-import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 import android.widget.EditText;
-import android.widget.ListView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import edu.pitt.cs.cs1635.mypantry.adapters.GListItemAdapter;
-import edu.pitt.cs.cs1635.mypantry.model.GListItem;
+import edu.pitt.cs.cs1635.mypantry.model.Item;
+import edu.pitt.cs.cs1635.mypantry.model.ItemDao;
 
 
-public class GroceryListActivity extends BaseActivity implements AddgListItemDialogFragment.AddListItemDialogListener{
+public class GroceryListActivity extends BaseActivity implements AddgListItemDialogFragment.AddListItemDialogListener {
 
-    public static Context context;
-    public static ArrayList<GListItem> listitems = new ArrayList<GListItem>();
-    public static ArrayAdapter adapter;
+    GListItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,7 +33,6 @@ public class GroceryListActivity extends BaseActivity implements AddgListItemDia
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar1);
         setSupportActionBar(toolbar);
 
-        context=this;
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.add_btn);
 
         fab.setOnClickListener(new View.OnClickListener() {
@@ -56,43 +52,16 @@ public class GroceryListActivity extends BaseActivity implements AddgListItemDia
 
         initNavigationDrawer();
 
-        if(listitems.size()==0){
-            populateList();
-        }
-
-        adapter= new GListItemAdapter(this,listitems);
-                //ArrayAdapter<GListItem>(this, R.layout.list,listitems);
-        ListView lv = (ListView)findViewById(R.id.g_list);
-        lv.setAdapter(adapter);
-        lv.setClickable(false);
-
-
-
-        Snackbar.make(findViewById(R.id.glistContainer), "Grocery List -- tap + to add an item!", Snackbar.LENGTH_LONG).show();
-
-    }
-
-    private void populateList() {
-        //add stuff to the list here!
-        String one="Bread";
-        String two="Orange Juice";
-        String three="Bananas";
-        listitems.add(new GListItem(one,""));
-        listitems.add(new GListItem(three,""));
-        listitems.add(new GListItem(two,""));
+        notifyUser("Grocery List -- tap + to add an item!");
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-
-
-        return super.onOptionsItemSelected(item);
+    public void onResume() {
+        super.onResume();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.g_list_recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter= new GListItemAdapter(this.itemDao);
+        recyclerView.setAdapter(adapter);
     }
 
     public void showDialog(){
@@ -100,6 +69,13 @@ public class GroceryListActivity extends BaseActivity implements AddgListItemDia
         dialog.show(getFragmentManager(),"Tag");
     }
 
+    /**
+     * Show a SnackBar to notify the user that some event took place
+     * @param message the message to display
+     */
+    public void notifyUser(String message) {
+        Snackbar.make(findViewById(R.id.glistContainer), message,Snackbar.LENGTH_LONG).show();
+    }
 
     @Override
     public void onDialogPositiveClick(DialogFragment dialog) {
@@ -108,19 +84,28 @@ public class GroceryListActivity extends BaseActivity implements AddgListItemDia
         EditText edit = (EditText) dialogview.findViewById(R.id.dialog_new_item);
         if(edit!=null){
             String txt = edit.getText().toString();
-            if(txt.equals(""))
-                return;
-            if(listitems.contains(txt)){
-                Snackbar.make(findViewById(R.id.glistContainer),"Item is already in List!",Snackbar.LENGTH_LONG).show();
+            if(txt.equals("")) {
                 return;
             }
-            listitems.add(new GListItem(txt,""));
-            adapter.notifyDataSetChanged();
-            Snackbar.make(findViewById(R.id.glistContainer),"Added item: "+txt,Snackbar.LENGTH_LONG).show();
+            List<Item> matchingItems = this.itemDao.queryBuilder()
+                    .where(ItemDao.Properties.Title.eq(txt))
+                    .list();
+            if (matchingItems.size() > 0) {
+                Item item = matchingItems.get(0);
+                if (item.getOnGroceryList()) {
+                    notifyUser("Item is already in list!");
+                } else {
+                    item.setOnGroceryList(true);
+                    this.itemDao.update(item);
+                    adapter.notifyDataSetChanged();
+                    notifyUser("Added item: " + txt);
+                }
+            } else {
+                notifyUser("Could not find an item called " + txt);
+            }
         }else{
-            Log.d("MyPantry","Edittext no found!");
+            Log.d("MyPantry", "Edittext no found!");
         }
-
     }
 
     @Override
--- app/src/main/java/edu/pitt/cs/cs1635/mypantry/adapters/GListItemAdapter.java
+++ app/src/main/java/edu/pitt/cs/cs1635/mypantry/adapters/GListItemAdapter.java
@@ -1,45 +1,108 @@
 package edu.pitt.cs.cs1635.mypantry.adapters;
 
-import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.ArrayList;
+import java.util.List;
 
 import edu.pitt.cs.cs1635.mypantry.R;
-import edu.pitt.cs.cs1635.mypantry.model.GListItem;
+import edu.pitt.cs.cs1635.mypantry.model.Item;
+import edu.pitt.cs.cs1635.mypantry.model.ItemDao;
 
 /**
  * Created by Chris on 3/30/2016.
  */
-public class GListItemAdapter extends ArrayAdapter<GListItem> {
-    public GListItemAdapter(Context context, ArrayList<GListItem> items){
-        super(context,0,items);
+public class GListItemAdapter extends RecyclerView.Adapter<GListItemAdapter.ViewHolder> {
+
+    private List<Item> data;
+    private ItemDao itemDao;
+
+    public GListItemAdapter(ItemDao itemDao){
+        this.itemDao = itemDao;
+        loadData();
+
+        this.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onChanged() {
+                super.onChanged();
+                loadData();
+            }
+        });
+    }
+
+    public void loadData() {
+        this.data = this.itemDao.queryBuilder()
+                .where(ItemDao.Properties.OnGroceryList.eq(true))
+                .list();
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
-        GListItem it=getItem(position);
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View gListItemView = LayoutInflater
+                .from(parent.getContext())
+                .inflate(R.layout.glist_item, null);
 
-        if(convertView==null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.glist_item,parent,false);
+        ViewHolder viewHolder = new GListItemAdapter.ViewHolder(gListItemView, this);
+        return viewHolder;
+    }
 
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.setPantryItem(this.data.get(position));
+    }
+
+    public void removeItem(Item item) {
+        item.setOnGroceryList(false);
+        this.itemDao.update(item);
+        this.notifyDataSetChanged();
+    }
+
+    public void buyItem(Item item) {
+        item.setOnGroceryList(false);
+        item.setAmount(2);
+        this.itemDao.update(item);
+        this.notifyDataSetChanged();
+    }
+
+    @Override
+    public int getItemCount() {
+        return this.data.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        private Item pantryItem;
+        private TextView pantryItemName;
+
+        public ViewHolder(View itemView, final GListItemAdapter adapter) {
+            super(itemView);
+
+            this.pantryItemName = (TextView) itemView.findViewById(R.id.gList_item_name);
+
+            Button removeButton = (Button) itemView.findViewById(R.id.gList_item_remove_item);
+            removeButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapter.removeItem(pantryItem);
+                }
+            });
+
+            Button boughtButton = (Button) itemView.findViewById(R.id.gList_item_bought_item);
+            boughtButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    adapter.buyItem(pantryItem);
+                }
+            });
         }
 
-        TextView itemname=(TextView) convertView.findViewById(R.id.text_data);
-        itemname.setText(it.item);
-
-        if(it.amt.equals("LOW")){
-            itemname.setTextColor(0xFF293E);
-        }else if(it.amt.equals("MED")){
-            itemname.setTextColor(0xFFA229);
-        }else{
-            //itemname.setTextColor(0x000000);
+        protected void setPantryItem(Item pantryItem) {
+            this.pantryItem = pantryItem;
+            this.pantryItemName.setText(pantryItem.getTitle());
         }
-
-        return convertView;
     }
 }
